8ecad45b85bd8332182b3a9545188098587778f9
Tom <thomaspatricksteven@gmail.com>
2023-09-17 22:44:30 -0400

    dadbod

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
+local cmp = require("cmp")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "sql", "mysql", "plsql" },
+  callback = function()
+    cmp.setup.buffer({
+      sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" },
+        { name = "luasnip" },
+      },
+    })
+  end,
+  group = autocomplete_group,
+})
