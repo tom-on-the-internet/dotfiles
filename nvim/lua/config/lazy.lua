e9790e9b1944997ec63729ab58250ee5da073f7a
Tom <thomaspatricksteven@gmail.com>
2023-09-17 20:45:06 -0400

    minifiles

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
