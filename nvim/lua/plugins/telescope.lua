58bd9ccced4c948d67ed78197718654b88db5d21
Tom <thomaspatricksteven@gmail.com>
2024-05-07 22:21:08 -0400

    trivy

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,6 +3,12 @@ return {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             {
+                "ANGkeith/telescope-terraform-doc.nvim",
+                config = function()
+                    require("telescope").load_extension("terraform_doc")
+                end,
+            },
+            {
                 "isak102/telescope-git-file-history.nvim",
                 dependencies = { "tpope/vim-fugitive" },
                 config = function()
