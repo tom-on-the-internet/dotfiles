bfec1f1811a78ef6feacf8ec8762b08602502f4e
Tom <thomaspatricksteven@gmail.com>
2024-05-26 19:56:20 -0400

    nicer go

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,10 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
-vim.opt.swapfile = false
 vim.opt.conceallevel = 0 -- don't hide my json strings
-vim.opt.spell = false -- I use cspell
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
 vim.opt.cursorline = false
+vim.opt.list = false -- don't show tabs
+vim.opt.relativenumber = false
+vim.opt.shiftwidth = 4
+vim.opt.spell = false -- I use cspell
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
