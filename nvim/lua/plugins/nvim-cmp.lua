b06e3f539c377ef5dfbc840d42c1667ba9b7d7b2
Tom <thomaspatricksteven@gmail.com>
2024-05-29 08:18:07 -0400

    better completion

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,24 @@
+return {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+        local cmp = require("cmp")
+        opts.preselect = cmp.PreselectMode.None
+        opts.completion = {
+            completeopt = "menu,menuone,noinsert,noselect",
+        }
+
+        opts.mapping = cmp.mapping.preset.insert({
+            ["<C-n>"] = cmp.mapping.select_next_item({
+                behavior = cmp.SelectBehavior.Insert,
+            }),
+            ["<C-p>"] = cmp.mapping.select_prev_item({
+                behavior = cmp.SelectBehavior.Insert,
+            }),
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<C-y>"] = LazyVim.cmp.confirm({ select = false }),
+        })
+        opts.experimental = { ghost_text = false }
+    end,
+}
