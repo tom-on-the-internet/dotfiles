b02db1298ade3b5806af2a39b72bc1b75d36b91a
Tom <thomaspatricksteven@gmail.com>
2024-07-07 21:20:07 -0400

    gruv

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,17 @@
 return {
     {
-        "rebelot/kanagawa.nvim",
+        "sainnhe/gruvbox-material",
+
         version = false,
         lazy = false,
         priority = 1000,
         config = function()
-            require("kanagawa").setup({
-                colors = {
-                    theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none",
-                            },
-                        },
-                    },
-                },
-            })
-
             -- remove soon
             vim.o.background = "light"
 
-            vim.cmd.colorscheme("kanagawa")
+            vim.g.gruvbox_material_enable_italic = true
+
+            vim.cmd.colorscheme("gruvbox-material")
         end,
     },
 }
--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -33,36 +33,12 @@ return {
                 header = vim.split(logo, "\n"),
         -- stylua: ignore
         center = {
-        { action = 'lua LazyVim.pick()()',   desc = "0 Code",  icon = " ", key = "c",key_format = ' %s' },
-        -- { action = "ene | startinsert",                              desc = " New File",        icon = " ", key = "n" },
-        -- { action = 'lua LazyVim.pick("oldfiles")()',                 desc = " Recent Files",    icon = " ", key = "r" },
-        -- { action = 'lua LazyVim.pick("live_grep")()',                desc = " Find Text",       icon = " ", key = "g" },
-        -- { action = 'lua LazyVim.pick.config_files()()',              desc = " Config",          icon = " ", key = "c" },
-        -- { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-        -- { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
-        -- { action = "Lazy",                                           desc = " Lazy",            icon = "󰒲 ", key = "l" },
-        { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = "  Give Up",   icon = "󰩈 ", key = "q",key_format = ' %s'  },
+        { action = 'lua LazyVim.pick()()',   desc = " Code",  icon = " ", key = "c",key_format = ' %s' },
+        { action = function() vim.api.nvim_input("<cmd>qa<cr>") end, desc = " Give Up",   icon = "󰩈 ", key = "q",key_format = ' %s'  },
         },
                 footer = {},
-                -- footer = function()
-                --     local stats = require("lazy").stats()
-                --     local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-                --     return {
-                --         stats.loaded
-                --             .. "/"
-                --             .. stats.count
-                --             .. " plugins in "
-                --             .. ms
-                --             .. "ms",
-                --     }
-                -- end,
             },
         }
-
-        -- for _, button in ipairs(opts.config.center) do
-        --     button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        --     button.key_format = "  %s"
-        -- end
 
         -- open dashboard after closing lazy
         if vim.o.filetype == "lazy" then
