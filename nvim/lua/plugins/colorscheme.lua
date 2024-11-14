7bba12e13d02068dcfc1edfacf9bb2ec4ea772ef
Tom <thomaspatricksteven@gmail.com>
2024-11-14 18:03:00 -0500

    wip

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,36 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "catppuccin-frappe",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "frappe", -- latte, frappe, macchiato, mocha
+        transparent_background = false,
+        integrations = {
+          aerial = true,
+          grug_far = true,
+          mason = true,
+          noice = true,
+          notify = true,
+          lsp_trouble = true,
+          dadbod_ui = true,
+        },
+      })
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
     },
   },
 }
