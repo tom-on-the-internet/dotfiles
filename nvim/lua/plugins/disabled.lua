45ca7284282a38d8e82463f7e7b23fbf333c05a7
Tom <thomaspatricksteven@gmail.com>
2024-12-08 20:19:37 -0500

    disable

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.animate", enabled = false },
 }
