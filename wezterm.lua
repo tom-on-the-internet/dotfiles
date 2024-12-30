df936d211ba395b3bdbacc0d6ecddb00ee48d302
Tom <thomaspatricksteven@gmail.com>
2024-12-29 22:09:34 -0500

    ghostty

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,8 @@ return {
 	adjust_window_size_when_changing_font_size = false,
 	audible_bell = "Disabled",
 	-- color_scheme = "Catppuccin Mocha",
-	color_scheme = "Night Owl (Gogh)",
+	color_scheme = "kanagawabones",
+	-- color_scheme = "Kanagawa Dragon (Gogh)",
 	enable_tab_bar = false,
 	-- font = wezterm.font({ family = "IBM Plex Mono" }),
 	font = wezterm.font({ family = "Berkeley Mono" }),
@@ -48,6 +49,43 @@ return {
 			key = "B",
 			mods = "CTRL",
 			action = wezterm.action.EmitEvent("toggle-opacity"),
+		},
+	},
+	--dragon
+	force_reverse_video_cursor = true,
+	colors = {
+		foreground = "#c5c9c5",
+		background = "#181616",
+
+		cursor_bg = "#C8C093",
+		cursor_fg = "#C8C093",
+		cursor_border = "#C8C093",
+
+		selection_fg = "#C8C093",
+		selection_bg = "#2D4F67",
+
+		scrollbar_thumb = "#16161D",
+		split = "#16161D",
+
+		ansi = {
+			"#0D0C0C",
+			"#C4746E",
+			"#8A9A7B",
+			"#C4B28A",
+			"#8BA4B0",
+			"#A292A3",
+			"#8EA4A2",
+			"#C8C093",
+		},
+		brights = {
+			"#A6A69C",
+			"#E46876",
+			"#87A987",
+			"#E6C384",
+			"#7FB4CA",
+			"#938AA9",
+			"#7AA89F",
+			"#C5C9C5",
 		},
 	},
 }
