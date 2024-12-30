77521f9c92810700cbaa887905d3b873322b3830
Tom <thomaspatricksteven@gmail.com>
2024-12-30 15:33:08 -0500

    wip

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,6 +1,13 @@
+---@module "lazy"
+---@type LazySpec
 return {
   {
     "saghen/blink.cmp",
+    dependencies = {
+      "mikavilpas/blink-ripgrep.nvim",
+    },
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
       completion = {
         accept = {
@@ -18,11 +25,23 @@ return {
         },
       },
       sources = {
-        -- adding any nvim-cmp sources here will enable them
-        -- with blink.compat
         compat = {},
-        default = { "lsp", "path", "snippets", "buffer" },
+        default = { "lsp", "path", "snippets", "buffer", "ripgrep" },
         cmdline = {},
+        providers = {
+          ripgrep = {
+            module = "blink-ripgrep",
+            name = "Ripgrep",
+            -- the options below are optional, some default values are shown
+            ---@module "blink-ripgrep"
+            ---@type blink-ripgrep.Options
+            opts = {
+              prefix_min_len = 8,
+              context_size = 5,
+              max_filesize = "100K",
+            },
+          },
+        },
       },
       keymap = {
         preset = "default",
