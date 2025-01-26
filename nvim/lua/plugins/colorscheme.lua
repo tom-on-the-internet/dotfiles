edbf6700ad131c0ac8c5cc0f46b308358f2defd7
Tom <thomaspatricksteven@gmail.com>
2025-01-25 22:44:40 -0500

    wip

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -26,6 +26,19 @@ return {
     end,
   },
   {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        disable_background = true,
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      })
+    end,
+  },
+  {
     "folke/noice.nvim",
     opts = {
       presets = {
