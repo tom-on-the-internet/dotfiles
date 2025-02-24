c02d6dee454200ddf957b5140ddd78edfe2d3d6e
Tom <thomaspatricksteven@gmail.com>
2025-02-24 16:58:08 -0500

    wip

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -3,9 +3,6 @@
 return {
   {
     "saghen/blink.cmp",
-    dependencies = {
-      "mikavilpas/blink-ripgrep.nvim",
-    },
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
@@ -26,21 +23,7 @@ return {
       },
       sources = {
         compat = {},
-        default = { "lsp", "path", "snippets", "buffer", "ripgrep" },
-        providers = {
-          ripgrep = {
-            module = "blink-ripgrep",
-            name = "Ripgrep",
-            -- the options below are optional, some default values are shown
-            ---@module "blink-ripgrep"
-            ---@type blink-ripgrep.Options
-            opts = {
-              prefix_min_len = 5,
-              context_size = 5,
-              max_filesize = "100K",
-            },
-          },
-        },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
       keymap = {
         preset = "default",
