3aa39e1eb06d488891ce9928eb16069df1eee3e7
Tom <thomaspatricksteven@gmail.com>
2025-04-11 21:26:50 -0400

    nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,30 +13,5 @@ return {
       },
     },
   },
-
-  { "projekt0n/github-nvim-theme", name = "github-theme" },
-  { "tiagovla/tokyodark.nvim" },
-  { "navarasu/onedark.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "marko-cerovac/material.nvim" },
-  {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  { "sainnhe/sonokai" },
-  { "AlexvZyl/nordic.nvim" },
-
   { "Mofiqul/vscode.nvim" },
-  {
-    "slugbyte/lackluster.nvim",
-    lazy = false,
-    priority = 1000,
-    init = function()
-      -- vim.cmd.colorscheme("lackluster")
-      -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-      -- vim.cmd.colorscheme("lackluster-mint")
-    end,
-  },
 }
