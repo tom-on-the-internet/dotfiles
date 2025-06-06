ddd101bc384b73da634da4933f6d68b48ae4191a
Tom <thomaspatricksteven@gmail.com>
2025-06-06 12:04:31 -0400

    no tailwind

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -9,6 +9,15 @@ return {
         jsonls = {},
         html = {},
         emmet_language_server = {},
+        -- tailwindcss = {
+        --   hovers = true,
+        --   suggestions = true,
+        --   root_dir = function(fname)
+        --     local root_pattern =
+        --       require("lspconfig").util.root_pattern("tailwind.config.cjs", "tailwind.config.js", "postcss.config.js")
+        --     return root_pattern(fname)
+        --   end,
+        -- },
         intelephense = {
           init_options = {
             licenceKey = "00G0KD8UUF391H9",
