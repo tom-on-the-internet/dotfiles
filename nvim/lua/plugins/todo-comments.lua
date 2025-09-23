cf51ed9003fe1e1e8d861cb96a288dc40640b411
Tom <thomaspatricksteven@gmail.com>
2025-09-22 21:32:02 -0400

    update

---
+++ nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      keywords = {
+        MUSTDO = { icon = " ", color = "warning" },
+      },
+    },
+  },
+}
