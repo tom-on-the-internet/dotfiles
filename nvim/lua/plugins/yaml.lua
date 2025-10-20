cc4e9b2ec9ddb31e9e824d0e0401d4c62166e4bd
Tom <thomaspatricksteven@gmail.com>
2025-10-20 08:50:22 -0400

    update

---
+++ nvim/lua/plugins/yaml.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "Einenlum/yaml-revealer",
+    config = function()
+      -- Recommended for Neovim users
+      vim.g.yaml_revealer_display_mode = "virtual"
+    end,
+    ft = "yaml",
+  },
+}
