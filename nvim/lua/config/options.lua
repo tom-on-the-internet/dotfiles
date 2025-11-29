cc471c0d8599e5b8859c5a37b9fb14b7d675461f
Tom <thomaspatricksteven@gmail.com>
2025-11-28 22:25:22 -0500

    list

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -10,3 +10,8 @@ vim.opt.cursorline = false
 vim.opt.scrolloff = 10 -- Lines of context
 vim.g.lazyvim_php_lsp = "intelephense"
 vim.g.ai_cmp = false
+-- show trailing spaces but hide tabs
+vim.opt.listchars = {
+  tab = "  ", -- Represents a tab with two actual space characters, making it invisible.
+  trail = "·",
+}
