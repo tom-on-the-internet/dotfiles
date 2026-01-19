23156094d16548765fc83a7476c49350e5a040a3
Tom <thomaspatricksteven@gmail.com>
2026-01-19 08:16:04 -0500

    vague

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,8 +3,6 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "kanagawa-paper-ink",
-      -- colorscheme = "tokyonight-storm",
-      -- colorscheme = "rose-pine",
     },
   },
   {
@@ -37,11 +35,15 @@ return {
     },
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-  },
-
-  {
-    "https://codeberg.org/trondelag/CuteScheme",
+    "vague-theme/vague.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other plugins
+    config = function()
+      -- NOTE: you do not need to call setup if you don't want to.
+      require("vague").setup({
+        -- optional configuration here
+      })
+      vim.cmd("colorscheme vague")
+    end,
   },
 }
