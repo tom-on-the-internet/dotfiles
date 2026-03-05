8a7b9421cceb2c6c3df582c45708e751b785f8f2
Tom <thomaspatricksteven@gmail.com>
2026-03-04 22:37:07 -0500

    tidy up

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,23 +6,6 @@ return {
     },
   },
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-
-  {
-    "bluz71/vim-moonfly-colors",
-    name = "moonfly",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.moonflyItalics = false
-    end,
-  },
-
-  {
     "folke/noice.nvim",
     opts = {
       presets = {
@@ -31,18 +14,12 @@ return {
     },
   },
   {
-    "thesimonho/kanagawa-paper.nvim",
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
     lazy = false,
     priority = 1000,
     config = function()
-      require("kanagawa-paper").setup({
-        -- transparent = true,
-        diag_background = false,
-        styles = {
-          comment = { italic = false },
-        },
-      })
-      -- vim.cmd("colorscheme kanagawa-paper-ink")
+      vim.g.moonflyItalics = false
     end,
   },
 }
