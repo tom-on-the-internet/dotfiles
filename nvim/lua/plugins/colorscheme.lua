0315d7628fbd09d50c3acf253dc2ce7740959737
Tom <thomaspatricksteven@gmail.com>
2026-03-24 23:03:36 -0400

    macchiato

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "moonfly",
+      colorscheme = "catppuccin-macchiato",
     },
   },
   {
@@ -12,6 +12,16 @@ return {
         lsp_doc_border = true,
       },
     },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        no_italic = true, -- Force no italic
+      })
+    end,
   },
   {
     "bluz71/vim-moonfly-colors",
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
-    theme = "moonfly",
     sections = {
       lualine_a = {
         {
